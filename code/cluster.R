844ef444cd32034eaf209973929fde0fe755c1c1
Patrick Perry <patperry@gmail.com>
2015-06-10 22:49:05 -0400

    fix issues with deleting .Random.seed in cluster_kmeans

--- code/cluster.R
+++ code/cluster.R
@@ -5,7 +5,7 @@
 cluster_kmeans <- function(x, centers, seed = 2651513, nstart = 100, ...)
 {
     if (!is.na(seed)) {
-        if ((exists0 <- exists(".Random.seed", globalenv()))) {
+        if ((exists0 <- exists(".Random.seed", envir=globalenv()))) {
             seed0 <- .Random.seed
         }
         set.seed(seed)
@@ -15,13 +15,11 @@ cluster_kmeans <- function(x, centers, seed = 2651513, nstart = 100, ...)
 
     if (!is.na(seed)) {
         if (exists0) {
-            .Random.seed <- seed0
-        } else {
-            rm(".Random.seed", globalenv())
+            assign(".Random.seed", seed0, envir=globalenv())
+        } else if (exists(".Random.seed", envir=globalenv())) {
+            rm(".Random.seed", envir=globalenv())
         }
     }
 
     cl
 }
-
-
