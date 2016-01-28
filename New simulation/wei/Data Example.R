254699f65c77115a7dbcdb5904353b81893da1d6
fuweiboy1988 <weifu.scott@gmail.com>
2016-01-27 22:24:50 -0500

    Update real data example--high dimension data

--- New simulation/wei/Data Example.R
+++ New simulation/wei/Data Example.R
@@ -1,4 +1,5 @@
 
+
    library("bcv")
    library("devtools")
    library("cluster")
@@ -15,6 +16,8 @@
    source("../code/jump.R")
    source("../code/wold.R")
 
+
+######------------Voting data-----------------------############
 Vote <- read.table("Voting.txt",sep=",")
 
 Id <- vector()
@@ -41,42 +44,42 @@ for(i in 1:232){
 
 set.seed(3)
 ID <- sample(1:232, 232)
-
 Vote <- VV[ID,]
-set.seed(0)
+
+set.seed(1)
 cv.kmeans.gabriel(Vote, 5, 2, maxcenters=10, classify.method="nearest")$centers
 
-set.seed(0)		
+set.seed(1)		
 gabriel_cor_correct(Vote, maxcenters=10, type = 2)
 
-set.seed(0)		
+set.seed(1)		
 Wold_holdout(data = Vote, CV = 5, Errortol = 0.01,  max.k =10)
 
-set.seed(0)		
+set.seed(1)		
 Gap <- clusGap(Vote, FUN = kmeans, K.max =10)
 which(Gap[[1]][,3] == max(Gap[[1]][,3]))
 			
-set.seed(0)	
+set.seed(1)	
 mcluster <- Mclust(Vote, G = 1:10)
  mcluster$G
 						
-set.seed(0)
+set.seed(1)
 Ch <- NbClust(Vote, min.nc = 2, max.nc = 10,method = "kmeans", index = "ch")
 Ch$Best.nc[1]
 	
-set.seed(0)
+set.seed(1)
 Hartigan <- NbClust(Vote, min.nc = 2, max.nc = 10,method = "kmeans", index = "hartigan")
 Hartigan$Best.nc[1]
 			
-set.seed(0)
-Jump <- jump(Vote,plotjumps=F,rand=10,trace=F)
+set.seed(1)
+Jump <- jump(Vote,10, plotjumps=FALSE, trace=FALSE)
 Jump$maxjump
 	
-set.seed(0)
+set.seed(1)
 PS <- prediction.strength(Vote, Gmin=2, Gmax=10)
  PS$optimalk
 			
-set.seed(0)
+set.seed(1)
 SB <- nselectboot(Vote,clustermethod=kmeansCBI,classification="centroid",krange=2:10)
 SB$kopt
 
@@ -105,95 +108,108 @@ set.seed(3)
 ID <- sample(1:683, 683)
 Breast <- VV[ID,1:9]
 
-set.seed(0)
+set.seed(1)
 cv.kmeans.gabriel(Breast, 5, 2, maxcenters=10, classify.method="nearest")$centers
 
-set.seed(0)		
+set.seed(1)		
 gabriel_cor_correct(Breast, maxcenters=10, type = 2)
 
-set.seed(0)		
+set.seed(1)		
 Wold_holdout(data = Breast, CV = 5, Errortol = 0.01,  max.k =10)
 
-set.seed(0)		
+set.seed(1)		
 Gap <- clusGap(Breast, FUN = kmeans, K.max =10)
 which(Gap[[1]][,3] == max(Gap[[1]][,3]))
 			
-set.seed(0)	
+set.seed(1)	
 mcluster <- Mclust(Breast, G = 1:10)
  mcluster$G
 						
-set.seed(0)
+set.seed(1)
 Ch <- NbClust(Breast, min.nc = 2, max.nc = 10,method = "kmeans", index = "ch")
 Ch$Best.nc[1]
 	
-set.seed(0)
+set.seed(1)
 Hartigan <- NbClust(Breast, min.nc = 2, max.nc = 10,method = "kmeans", index = "hartigan")
 Hartigan$Best.nc[1]
 			
-set.seed(0)
-Jump <- jump(Breast,plotjumps=F,rand=10,trace=F)
+set.seed(1)
+Jump <- jump(Breast,10, plotjumps=FALSE, trace=FALSE)
 Jump$maxjump
 	
-set.seed(0)
+set.seed(1)
 PS <- prediction.strength(Breast, Gmin=2, Gmax=10)
  PS$optimalk
 			
-set.seed(0)
+set.seed(1)
 SB <- nselectboot(Breast,clustermethod=kmeansCBI,classification="centroid",krange=2:10)
 SB$kopt
 
 ###-----------------------------------------------------------------------------
+DATA <- read.table("promeroy.txt", header = T, sep="\t")
 
-Data <- read.table("sonar.txt",sep=",")
-
-MM <- matrix(NA,208,61)
-for(i in 1:208){
-	for(j in 1:61){
-		MM[i,j] = Data[i,j]
+Data <- matrix(NA,1379,42)
+for(i in 1:1379){
+	for(j in 1:42){
+		Data[i,j] = as.numeric(as.character(DATA[i+1,j+1]))
 	}
 }
 
-set.seed(3)
-Id <- sample(1:208,208)
+DATA <- t(Data)
 
-Sonar <- MM[Id,1:60]
+set.seed(1)
+cv.kmeans.gabriel(DATA, 5, 2, maxcenters=10, classify.method="nearest")$centers
 
-set.seed(0)
-cv.kmeans.gabriel(Sonar, 5, 2, maxcenters=10, classify.method="nearest")$centers
+set.seed(1)		
+gabriel_cor_correct(DATA, maxcenters=10, type = 2)
 
-set.seed(0)		
-gabriel_cor_correct(Sonar, maxcenters=10, type = 2)
+set.seed(1)		
+Wold_holdout(data = DATA, CV = 5, Errortol = 0.01,  max.k =10)
 
-set.seed(0)		
-Wold_holdout(data = Sonar, CV = 5, Errortol = 0.01,  max.k =10)
-
-set.seed(0)		
-Gap <- clusGap(Sonar, FUN = kmeans, K.max =10)
+set.seed(1)		
+Gap <- clusGap(DATA, FUN = kmeans, K.max =10)
 which(Gap[[1]][,3] == max(Gap[[1]][,3]))
 			
-set.seed(0)	
-mcluster <- Mclust(Sonar, G = 1:10)
+set.seed(1)	
+mcluster <- Mclust(DATA, G = 1:10)
  mcluster$G
 						
-set.seed(0)
-Ch <- NbClust(Sonar, min.nc = 2, max.nc = 10,method = "kmeans", index = "ch")
+set.seed(1)
+Ch <- NbClust(DATA, min.nc = 2, max.nc = 10,method = "kmeans", index = "ch")
 Ch$Best.nc[1]
 	
-set.seed(0)
-Hartigan <- NbClust(Sonar, min.nc = 2, max.nc = 10,method = "kmeans", index = "hartigan")
+set.seed(1)
+Hartigan <- NbClust(DATA, min.nc = 2, max.nc = 10,method = "kmeans", index = "hartigan")
 Hartigan$Best.nc[1]
 			
-set.seed(0)
-Jump <- jump(Sonar,plotjumps=F,rand=10,trace=F)
+set.seed(1)
+Jump <- jump(DATA,10, plotjumps=FALSE, trace=FALSE)
 Jump$maxjump
 	
-set.seed(0)
-PS <- prediction.strength(Sonar, Gmin=2, Gmax=10)
+set.seed(1)
+PS <- prediction.strength(DATA, Gmin=2, Gmax=10)
  PS$optimalk
 			
-set.seed(0)
-SB <- nselectboot(Sonar,clustermethod=kmeansCBI,classification="centroid",krange=2:10)
+set.seed(1)
+SB <- nselectboot(DATA,clustermethod=kmeansCBI,classification="centroid",krange=2:10)
 SB$kopt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
